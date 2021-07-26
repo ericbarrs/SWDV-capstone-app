7a7165e327b58b58e6c12081958179b984a06399
Eric <yellastar42@yahoo.com>
2021-07-25 23:08:17 -0500

    update

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,16 +1,16 @@
-export const test = () =>{
+export const test = () => {
     return function (dispatch) {
         fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.json())
-			.then((post) => {
-				dispatch(testing(post));
-			})
-    }
-}
+            .then((res) => res.json())
+            .then((post) => {
+                dispatch(testing(post));
+            });
+    };
+};
 
-const testing = (post) =>{
+const testing = (post) => {
     return {
-		type: "testing",
-		payload: post,
-	};
-}
+        type: "testing",
+        payload: post,
+    };
+};
